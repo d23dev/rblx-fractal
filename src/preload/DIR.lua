c1a8b8cbeca51673d79796e71b44e679c59e9793
angeld23 <scrapmechanickid@gmail.com>
2022-01-04 03:55:04 -0500

    Add DIR module to preload

---
+++ src/preload/DIR.lua
@@ -0,0 +1,27 @@
+--// DIR.lua
+--[[
+    Easy access to directories in the game.
+    Usage example:
+        local DIR = require(ReplicatedFirst.DIR)
+        local Knit = require(DIR.packages.Knit)
+        ...
+]]
+
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
+
+local function toNil(value)
+    if not value then
+        return nil
+    end
+end
+
+return {
+    client = player and player:WaitForChild("PlayerScripts"):WaitForChild("src");
+    server = toNil(not player) and game:GetService("ServerScriptService"):WaitForChild("src");
+    shared = game:GetService("ReplicatedStorage"):WaitForChild("src");
+    preload = game:GetService("ReplicatedFirst"):WaitForChild("src");
+    unreplicated = toNil(not player) and game:GetService("ServerStorage"):WaitForChild("src");
+    packages = game:GetService("ReplicatedStorage"):WaitForChild("Packages");
+}
